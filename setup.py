d7bae2d72fad9e755301fcf0c80ac0591ebc6eeb
louis <smallbanglouis@gmail.com>
2018-09-22 15:36:08 +0800

    ADD: dependencies

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@ from distutils.core import setup
 setup(
     name='Minetorch',
     description='A tools collection for pytorch users',
-    version='0.1.6',
+    version='0.1.7',
     packages=['minetorch'],
     url="https://github.com/louis-she/minetorch",
     author='louis',
     author_email='chenglu.she@gmail.com',
-    keywords='pytorch minecraft'
+    keywords='pytorch minecraft',
+    install_requires=['tensorboardX', 'torch']
+
 )
 
