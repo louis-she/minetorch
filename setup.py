9f1e910afd1c8603c70345bbfe518c9585f6e8ac
chenglu <chenglu.she@gmail.com>
2019-09-06 17:18:13 +0800

    update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name='Minetorch',
     description='A tools collection for pytorch users',
-    version='0.3.9',
+    version='0.4.0',
     packages=['minetorch'],
     url="https://github.com/louis-she/minetorch",
     author='louis',
