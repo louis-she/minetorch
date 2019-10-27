484758ff2a0c913b2a7abe858fc57ec72dd335bb
pengbo0054 <pengbo0054@gmail.com>
2019-10-27 12:24:31 +0800

    update metrics

--- minetorch/metrics.py
+++ minetorch/metrics.py
@@ -11,7 +11,7 @@ def iou(logits, targets, threshold=0.5, separate_class=False):
     intersection = (logits & targets).double().sum((1, 2))
     union = (logits | targets).double().sum((1, 2))
     iou = (intersection + 1e-7) / (union + 1e-7)
-    if separate_class_:
+    if separate_class:
         iou = iou
     else:
         iou = iou.mean()
@@ -31,7 +31,7 @@ def dice(logits, targets, threshold=0.5, separate_class=False):
     A = logits.double().sum((1, 2))
     B = targets.double().sum((1, 2))
     dice = 2 * (intersection + 1e-7) / (A + B + 1e-7)
-    if separate_class_:
+    if separate_class:
         dice = dice
     else:
         dice = dice.mean()
