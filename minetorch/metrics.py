934efeda272741df26d829b8cb58957e74641d12
pengbo0054 <pengbo0054@gmail.com>
2019-10-29 17:35:01 +0800

    update metircs

--- minetorch/metrics.py
+++ minetorch/metrics.py
@@ -3,7 +3,7 @@ import torch
 import functools
 
 
-def metric_utils(logits, targets, threshold=0.5):
+def shape_norm(logits, targets):
     # Expect one of the following shapes [classes, height, width], [height, width], [classes].
     assert logits.shape == targets.shape, "Inputs not match with targets on shape"
     assert len(logits.shape) <= 3, 'Expect one of the following shapes [classes, height, width], [height, width], [classes].'
@@ -14,13 +14,13 @@ def metric_utils(logits, targets, threshold=0.5):
         logits = logits.unsqueeze(0)
         targets = targets.unsqueeze(0)
     logits = torch.sigmoid(logits)
-    logits = logits > threshold
-    targets = targets > 0.5
     return logits, targets
 
 
 def iou(logits, targets, threshold=0.5, separate_class=False):
-    logits, targets = metric_utils(logits, targets, threshold=threshold)
+    logits, targets = shape_norm(logits, targets)
+    logits = logits > threshold
+    targets = targets > 0.5
     intersection = (logits & targets).double().sum((1, 2))
     union = (logits | targets).double().sum((1, 2))
     iou = (intersection + 1e-7) / (union + 1e-7)
@@ -36,7 +36,9 @@ multi_class_iou = functools.partial(iou, separate_class=True)
 
 
 def dice(logits, targets, threshold=0.5, separate_class=False):
-    logits, targets = metric_utils(logits, targets, threshold=threshold)
+    logits, targets = shape_norm(logits, targets)
+    logits = logits > threshold
+    targets = targets > 0.5
     intersection = (logits & targets).double().sum((1, 2))
     A = logits.double().sum((1, 2))
     B = targets.double().sum((1, 2))
@@ -53,7 +55,9 @@ multi_class_dice = functools.partial(dice, separate_class=True)
 
 
 def accuracy(logits, targets, threshold=0.5, separate_class=False):
-    logits, targets = metric_utils(logits, targets, threshold=threshold)
+    logits, targets = shape_norm(logits, targets)
+    logits = logits > threshold
+    targets = targets > 0.5
     true_prediction = (~(logits ^ targets)).double().sum((1, 2))
     total = logits.view(logits.shape[0], -1).shape[1]
     acc = true_prediction / total
@@ -70,10 +74,16 @@ multi_class_accuracy = functools.partial(accuracy, separate_class=True)
 
 def precision(logits, targets, threshold=0.5, separate_class=False):
     smooth = 1e-7
-    logits, targets = metric_utils(logits, targets, threshold=threshold)
+    logits, targets = shape_norm(logits, targets)
+    logits = logits > threshold
+    targets = targets > 0.5
     tp = ((logits == 1) & (targets == 1)).double().sum((1, 2))
     fp = ((logits == 1) & (targets == 0)).double().sum((1, 2))
     prec = tp / (tp + fp + smooth)
+    if separate_class:
+        prec = prec
+    else:
+        prec = prec.mean()
     return prec
 
 
@@ -83,12 +93,50 @@ multi_class_precission = functools.partial(precision, separate_class=True)
 
 def recall(logits, targets, threshold=0.5, separate_class=False):
     smooth = 1e-7
-    logits, targets = metric_utils(logits, targets, threshold=threshold)
+    logits, targets = shape_norm(logits, targets)
+    logits = logits > threshold
+    targets = targets > 0.5
     tp = ((logits == 1) & (targets == 1)).double().sum((1, 2))
     fn = ((logits == 0) & (targets == 1)).double().sum((1, 2))
     recall = tp / (tp + fn + smooth)
+    if separate_class:
+        recall = recall
+    else:
+        recall = recall.mean()
     return recall
 
 
 single_class_recall = functools.partial(recall, separate_class=False)
 multi_class_recall = functools.partial(recall, separate_class=True)
+
+
+def mse(logits, targets, separate_class=False):
+    logits, targets = shape_norm(logits, targets)
+    error = (abs(logits - targets) ** 2).double().sum((1, 2))
+    total = logits.view(logits.shape[0], -1).shape[1]
+    mse = error / total
+    if separate_class:
+        mse = mse
+    else:
+        mse = mse.mean()
+    return mse
+
+
+single_class_mse = functools.partial(mse, separate_class=False)
+multi_class_mse = functools.partial(mse, separate_class=True)
+
+
+def mae(logits, targets, separate_class=False):
+    logits, targets = shape_norm(logits, targets)
+    error = (abs(logits - targets)).double().sum((1, 2))
+    total = logits.view(logits.shape[0], -1).shape[1]
+    mae = error / total
+    if separate_class:
+        mae = mae
+    else:
+        mae = mae.mean()
+    return mae
+
+
+single_class_mae = functools.partial(mae, separate_class=False)
+multi_class_mae = functools.partial(mae, separate_class=True)
